1e42805e6d7feb6cba6b1dee8848df48c4a6c2f0
constt <175602385+constttdev@users.noreply.github.com>
2025-04-19 23:34:20 +0200

    feat: reminder embed

--- src/commands/reminding/addReminder.ts
+++ src/commands/reminding/addReminder.ts
@@ -14,17 +14,35 @@ export const data = new SlashCommandBuilder()
             .setDescription("The reminder to add")
             .setRequired(true)
             .setMinLength(1);
+    })
+    .addStringOption((option: SlashCommandStringOption) => {
+        return option
+            .setName("time")
+            .setDescription("When to remind you")
+            .setRequired(true)
+            .setMinLength(1);
     });
 
 export async function execute(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply();
+
     const reminder = interaction.options.getString("reminder");
+    const time = interaction.options.getString("time");
+
     try {
+        const embed = new EmbedBuilder().setTitle("Added reminder").addFields({
+            name: "Sucesfully added an reminder",
+            value: `${reminder} | ${time}`,
+            inline: false,
+        });
+        await interaction.editReply({ embeds: [embed] });
     } catch (error) {
         const embed = new EmbedBuilder().setTitle("Error").addFields({
             name: "An error occurred",
             value: "Failed to add reminder",
             inline: false,
         });
+        console.log(error);
         await interaction.reply({ embeds: [embed] });
     }
 }
