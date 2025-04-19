7e5e7947454b8279db9e1b01cb94ef1d0ad40a69
constt <175602385+constttdev@users.noreply.github.com>
2025-04-19 23:18:11 +0200

    feat: availible commands

---
+++ src/commands/utility/availibleCommands.ts
@@ -0,0 +1,49 @@
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("availiblecommands")
+  .setDescription("Replies with the avalible commands");
+
+const embed = new EmbedBuilder()
+  .setTitle("Currently availible commands")
+  .addFields(
+    {
+      name: "`/allreminders`",
+      value: "Displays all your reminders",
+      inline: false,
+    },
+    {
+      name: "`/addreminder <time> <reminder>`",
+      value: "Adds an reminder",
+      inline: false,
+    },
+    {
+      name: "`/removereminder <id>`",
+      value: "Removes an specific reminder",
+      inline: false,
+    },
+    {
+      name: "`/exportreminder <id>`",
+      value: "Exports an specific reminder",
+      inline: false,
+    },
+    {
+      name: "`/exportallreminder`",
+      value: "Exports all your reminders",
+      inline: false,
+    },
+    {
+      name: "`/nextreminder`",
+      value:
+        "Displays your next reminder ( Optionally you can dispaly multiple next Timers using the <timers> argument )",
+      inline: false,
+    }
+  );
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+  await interaction.reply({ embeds: [embed] });
+}
