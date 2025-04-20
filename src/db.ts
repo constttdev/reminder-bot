153f9a57c70453f5969b2cdce308fd4af5229044
constt <175602385+constttdev@users.noreply.github.com>
2025-04-20 20:05:45 +0200

    feat: file checks

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,11 @@
 import { JsonDB, Config } from "node-json-db";
+import fs from "fs/promises";
 
+export async function tryCatchFile() {
+    try {
+        await fs.access("./reminders.json");
+    } catch {
+        await fs.writeFile("./reminders.json", "{}", "utf-8");
+    }
+}
 export const db = new JsonDB(new Config("reminders", true));
